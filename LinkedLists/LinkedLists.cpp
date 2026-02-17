11c1a430ee3de7890d2d002f582bfc9d8b7f2506
FlavioIero <152231362+FlavioIero@users.noreply.github.com>
2026-02-17 17:22:10 +0100

    finire errore grave

--- LinkedLists/LinkedLists.cpp
+++ LinkedLists/LinkedLists.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <cstdlib>
 #include <vector>
+#include <set>
 using namespace std;
 
 
@@ -9,8 +10,8 @@ class SinglyLinkedList
 private: 
     struct Node
     {
-        int X = 0;
-        Node* Next = nullptr;
+        int x = 0;
+        Node* next = nullptr;
     };
     Node* _head = nullptr;
     unsigned int _size = 0;
@@ -32,7 +33,7 @@ public:
         {
             newNode = new Node{ (int)i, nullptr };
 
-            curr->Next = newNode;
+            curr->next = newNode;
             curr = newNode;
         }
 
@@ -45,7 +46,7 @@ public:
     { 
         if (_size == 0)
             throw out_of_range("List is empty");
-        return _head->X; 
+        return _head->x; 
     }
 #pragma endregion
 
@@ -69,8 +70,8 @@ public:
 
         while (curr != nullptr)
         {
-            next = curr->Next;
-            curr->Next = prev;
+            next = curr->next;
+            curr->next = prev;
             prev = curr;
             curr = next;
         }
@@ -86,8 +87,8 @@ public:
 
         while (currNode != nullptr)
         {
-            cout << currNode->X << ", ";
-            currNode = currNode->Next;
+            cout << currNode->x << ", ";
+            currNode = currNode->next;
         } 
 
         cout << "\n";
@@ -96,12 +97,12 @@ public:
     int get(unsigned int idx) const
     {
         Node* node = get_node(idx);
-        return node->X;
+        return node->x;
     }
 
     void set(int x, unsigned int idx)
     {
-        get_node(idx)->X = x;
+        get_node(idx)->x = x;
     }
 
     void insert(int x, unsigned int idx)
@@ -110,8 +111,8 @@ public:
             return push_front(x);
 
         Node* prev = get_node(idx - 1);
-        Node* node = new Node{ x, prev->Next };
-        prev->Next = node;
+        Node* node = new Node{ x, prev->next };
+        prev->next = node;
 
         _size++;
     }
@@ -129,14 +130,14 @@ public:
         {
             Node* prevHead = _head;
             _head = other._head;
-            otherTail->Next = prevHead;
+            otherTail->next = prevHead;
         }
         else
         {
             Node* prev = get_node(idx - 1);
-            Node* next = prev->Next;
-            prev->Next = other._head;
-            otherTail->Next = next;
+            Node* next = prev->next;
+            prev->next = other._head;
+            otherTail->next = next;
         }
 
         _size += otherSize;
@@ -171,8 +172,8 @@ public:
             return pop_front();
         
         Node* prev = get_node(idx - 1);
-        Node* curr = prev->Next;
-        prev->Next = curr->Next;
+        Node* curr = prev->next;
+        prev->next = curr->next;
         delete curr;
 
         _size--;
@@ -194,14 +195,14 @@ public:
         Node* beforeStart = &dummy;
 
         for (unsigned int i = 0; i < startIdx; i++)
-            beforeStart = beforeStart->Next;
+            beforeStart = beforeStart->next;
 
-        Node* curr = beforeStart->Next;
+        Node* curr = beforeStart->next;
 
         for (unsigned int i = startIdx; i < endIdx && curr != nullptr; i++)
         {
             Node* temp = curr;
-            curr = curr->Next;
+            curr = curr->next;
             delete temp;
             _size--;
         }
@@ -209,8 +210,8 @@ public:
         if (beforeStart == nullptr)
             throw logic_error("Error while removing element");
 
-        beforeStart->Next = curr;
-        _head = dummy.Next;
+        beforeStart->next = curr;
+        _head = dummy.next;
     }
 
     void pop_front()
@@ -219,7 +220,7 @@ public:
             throw out_of_range("List is empty");
 
         Node* prevHead = _head;
-        _head = _head->Next;
+        _head = _head->next;
         delete prevHead;
         _size--;
     }
@@ -233,12 +234,12 @@ public:
 
     int& operator[](unsigned int idx)
     {
-        return get_node(idx)->X;
+        return get_node(idx)->x;
     }
 
     const int& operator[](unsigned int idx) const
     {
-        return get_node(idx)->X;
+        return get_node(idx)->x;
     }
 
     void operator+=(SinglyLinkedList other)
@@ -256,22 +257,135 @@ public:
         Node* slow = _head;
         Node* fast = _head;
 
-        while (fast != nullptr && fast->Next != nullptr)
+        while (fast != nullptr && fast->next != nullptr)
         {
-            slow = slow->Next;
-            fast = fast->Next->Next;
+            slow = slow->next;
+            fast = fast->next->next;
         }
 
         // Defensive null-check to satisfy static analysis tools.
         if (slow == nullptr)
             throw logic_error("List is empty");
 
-        return slow->X;
+        return slow->x;
+    }
+
+    // da implementare ascending
+    void selection_sort(bool ascending = true)
+    {
+        if (empty())
+            return;
+
+        for (Node* l = _head; l != nullptr; l = l->next)
+        {
+            Node* replace = l;
+            for (Node* r = l->next; r != nullptr; r = r->next)
+            {
+                if (ascending)
+                {
+                    if (r->x < replace->x)
+                    {
+                        replace = r;
+                    }
+                }
+                else
+                {
+                    if (r->x > replace->x)
+                    {
+                        replace = r;
+                    }
+                }
+            }
+            if (replace != l)
+            {
+                int temp = l->x;
+                l->x = replace->x;
+                replace->x = temp;
+            }
+        }
+    }
+
+#pragma region static_methods
+    static SinglyLinkedList* merge_sorted_lists(SinglyLinkedList* l1, SinglyLinkedList* l2)
+    {
+        if ((l1 == nullptr || l1->empty()) && (l2 == nullptr || l2->empty()))
+            return nullptr;
+
+        SinglyLinkedList* res = new SinglyLinkedList();
+        Node* beforeHead = new Node();
+        Node* curr = beforeHead;
+
+        Node* h1 = l1->_head;
+        Node* h2 = l2->_head;
+
+        while (h1 && h2)
+        {
+            if (h1->x < h2->x)
+            {
+                curr->next = new Node{ h1->x, nullptr };
+                h1 = h1->next;
+            }
+            else
+            {
+                curr->next = new Node{ h2->x, nullptr };
+                h2 = h2->next;
+            }
+            curr = curr->next;
+        }
+
+        while (h1)
+        {
+            curr->next = new Node{ h1->x, nullptr };
+            h1 = h1->next;
+            curr = curr->next;
+        }
+
+        while (h2)
+        {
+            curr->next = new Node{ h2->x, nullptr };
+            h2 = h2->next;
+            curr = curr->next;
+        }
+
+        unsigned int size = l1->_size + l2->_size;
+        res->_head = beforeHead->next;
+        res->_size = size;
+
+        delete beforeHead;
+
+        return res;
+    }
+
+#pragma endregion
+
+    void delete_duplicates()
+    {
+        if (empty())
+            return;
+
+        Node* prev = nullptr;
+        Node* curr = _head;
+        Node* next = nullptr;
+        std::set<int> nums = { _head->x };
+
+        while (curr->next != nullptr)
+        {
+            if (nums.find(curr->next->x) == nums.end())
+            {
+                nums.insert(curr->next->x);
+                curr = curr->next;
+            }
+            else
+            {
+                Node* temp = curr->next;
+                curr->next = curr->next->next;
+                delete temp;
+            }
+        }
     }
 
     /* Cool exercises with singly linked lists include  
-    detecting cycles using Floyd’s Cycle-Finding Algorithm, 
-    finding the middle element in one pass, merging two sorted lists, 
+    detecting cycles using Floyd’s Cycle-Finding Algorithm,
     and reordering lists in zig-zag fashion.*/
 
 private:
@@ -284,7 +398,7 @@ private:
 
         for (unsigned int i = 0; i < idx; i++)
         {
-            curr = curr->Next;
+            curr = curr->next;
         }
 
         return curr;
@@ -292,12 +406,9 @@ private:
 };
 
 
-
 int main()
 {
-    vector<int> v = vector<int>();
-
-    v.clear();
+    vector<int> v = vector<int> { 3, 2, 2, 3 };
 
     int len = 7;
     int len2 = 4;
@@ -314,6 +425,24 @@ int main()
     //list.insert(list2, list.size());
     list += list2;
     list.print();
+
+    //list.selection_sort(false);
+    list.print();
+
+
+    cout << "List1: " << endl;
+    list.selection_sort();
+    list.print();
+    cout << "List2: " << endl;
+    list.selection_sort();
+    list2.print();
+    SinglyLinkedList* mergedList = SinglyLinkedList::merge_sorted_lists(&list, &list2);
+    cout << "Merged list: " << endl; 
+    mergedList[1] = 99;
+    mergedList[mergedList->size() - 1] = 99;
+    mergedList->print();
+    mergedList->delete_duplicates();
+    mergedList->print();
 
     return 0;
 }
